3efc0ce15e8823caf87020640cce57cd08542bb2
Nriver <6752679+Nriver@users.noreply.github.com>
2022-08-09 10:08:06 +0800

    升级版本号，尝试跨平台编译

--- QBRssManager.py
+++ QBRssManager.py
@@ -451,7 +451,7 @@ class App(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.title = 'qBittorrent 订阅下载规则管理 v1.2.0 by Nriver'
+        self.title = 'qBittorrent 订阅下载规则管理 v1.2.1 by Nriver'
         # 图标
         self.setWindowIcon(QtGui.QIcon(resource_path('QBRssManager.ico')))
         self.left = 0
