827db4d141992a944b43e8b8dab0bb8f83175f34
Nriver <6752679+Nriver@users.noreply.github.com>
2022-08-15 15:03:22 +0800

    优化异常信息输出

--- utils/pyqt_util.py
+++ utils/pyqt_util.py
@@ -1,11 +1,21 @@
+import traceback
+
 from PyQt5 import QtWidgets
+from loguru import logger
 
 
-def catch_exceptions(t, val, tb):
+def catch_exceptions(exc_type, exc_value, exc_tb):
     """获取pyqt5的exception"""
-    # old_hook = sys.excepthook
+
+    # 输出Traceback信息
+    tb = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
+    logger.error(f"error catched!:")
+    logger.error(f"error message:\n{tb}")
+
+    # 界面提示报错
     QtWidgets.QMessageBox.critical(None,
                                    "An exception was raised",
-                                   "Exception type: {}".format(t))
+                                   "Exception type: {}".format(exc_type))
     # 这里的要去掉 不然可能无限出发弹窗
+    # old_hook = sys.excepthook
     # old_hook(t, val, tb)
