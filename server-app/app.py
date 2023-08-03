e592333766bc3f8798d4f0a21db7e6a36f8d6239
Nriver <6752679+Nriver@users.noreply.github.com>
2023-08-03 11:14:46 +0800

    添加black配置

--- server-app/app.py
+++ server-app/app.py
@@ -8,15 +8,18 @@ CORS(app)
 # 假设有一个简单的数据列表
 data_list = [{'id': 1, 'name': 'Item 1'}, {'id': 2, 'name': 'Item 2'}]
 
+
 @app.route('/api/items', methods=['GET'])
 def get_items():
     return jsonify(data_list)
+
 
 @app.route('/api/items', methods=['POST'])
 def add_item():
     new_item = request.json
     data_list.append(new_item)
     return jsonify(new_item)
+
 
 if __name__ == '__main__':
     app.run()
